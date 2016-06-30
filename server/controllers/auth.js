56a42ab2d8b2cf8eb587c7701ce45b32398b11a3
future-challenger <sunshine_anycall@hotmail.com>
2016-06-30 23:20:30 +0800

    basic auth

---
+++ server/controllers/auth.js
@@ -0,0 +1,29 @@
+var passport =          require('passport'),
+    BasicStrategy =     require('passport-http').BasicStrategy,
+    User =              require('../models/user');
+
+passport.use(new BasicStrategy(
+    function(username, passpord, done) {
+        User.findOne({username: username}, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+
+            // 用户不存在
+            if (!user) {
+                return done(null, false);
+            }
+
+            // 检查用户的密码
+            user.verifyPassword(passowrd, function(err, match) {
+                // 密码不匹配
+                if (!match) {
+                    return done(null, false);
+                }
+
+                // 成功
+                return done(null, user);
+            });
+        });
+    }
+));
