d8921a021f8944e1eaee7b5b1288c6119867ae53
future-challenger <sunshine_anycall@hotmail.com>
2016-07-08 15:23:23 +0800

    promise

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -8,25 +8,35 @@ var postClients = function(req, res) {
     client.secret = req.body.secret;
     client.userId = req.user._id;
 
-    client.save(function(err) {
-        if (err) {
-            res.json({message: 'error', data: err});
-            return;
-        }
+    // client.save(function(err) {
+    //     if (err) {
+    //         res.json({message: 'error', data: err});
+    //         return;
+    //     }
 
-        res.json({message: 'done', data: client});
+    //     res.json({message: 'done', data: client});
+    // });
+    client.save().then(function(c) {
+        res.json({message: 'done', data: c});
+    }).catch(function(err) {
+        res.json({message: 'error', data: err});
     });
-
 };
 
 var getClients = function(req, res) {
-    Client.find({userId: req.user._id}, function(err, clients) {
-        if (err) {
-            res.json({messag: 'error', data: err});
-            return;
-        }
+    // Client.find({userId: req.user._id}, function(err, clients) {
+    //     if (err) {
+    //         res.json({messag: 'error', data: err});
+    //         return;
+    //     }
 
+    //     res.json({message: 'done', data: clients});
+    // });
+
+    Client.find({userId: req.user._id}).exec().then(function(clients) {
         res.json({message: 'done', data: clients});
+    }).catch(function(err) {
+        res.json({messag: 'error', data: err});
     });
 };
 
