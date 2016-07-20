70b25c8780dde0ba803bfd1077ac20ebbd966004
future-challenger <sunshine_anycall@hotmail.com>
2016-07-20 19:22:34 +0800

    base schema

--- server/models/base/index.js
+++ server/models/base/index.js
@@ -1,6 +1,23 @@
 var mongoose = require('mongoose'),
-    Promise = require('bluebird');
+    util     = require('util');
+    Promise  = require('bluebird');
     
 mongoose.Promise = Promise;
 
+var Schema = mongoose.Schema;
+
+function BaseSchema() {
+    Schema.apply(this, arguments);
+
+    this.add({
+        createdBy: { type: String, required: true },
+        createdAt: { type: Date, required: true, default: Date.now() },
+        updatedBy: { type: String, required: true },
+        updatedAt: { type: Date, required: true, default: Date.now() }
+    });
+}
+
+util.inherites(BaseSchema, Schema);
+
 module.exports = mongoose;
+module.exports.BaseSchema = BaseSchema;
