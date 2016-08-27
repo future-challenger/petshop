8bcac3f8c49749d933ff0c50b47a94a35139bca0
Sharper <sharp.bruce.lee@gmail.com>
2016-08-27 19:58:34 +0800

    react

--- server/petshop-admin/public/helloworld.js
+++ server/petshop-admin/public/helloworld.js
@@ -1,4 +1,52 @@
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <PagePic pagename={this.props.pagename} />
+                <PageLink pagename={this.props.pagename} />
+                <NodeList>
+                    <span>Jack</span>
+                    <span>Bruce</span>
+                    <span>KD</span>
+                </NodeList>
+            </div>
+        );
+    }
+});
+
+var PagePic = React.createClass({
+    render: function() {
+        return (
+            <img src={'https://graph.facebook.com/' + this.props.pagename + '/picture'} />
+        );
+    }
+});
+
+var PageLink = React.createClass({
+    render: function() {
+        return (
+            <a href={'https://www.facebook.com/' + this.props.pagename} >
+                {this.props.pagename}
+            </a>
+        );
+    }
+});
+
+// Node list
+var NodeList = React.createClass({
+    render: function() {
+        var nodeList = this.props.children.map(function(item) {
+            return <li>{item}</li>
+        });
+        return (
+            <ol>
+                {nodeList}
+            </ol>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>What's up!</h1>,
-    document.getElementById('example')
+    <Avatar pagename="Jack" />, 
+    document.getElementById('avatar')
 );
