8629eb05e3f0633fa808191b78779e4207eedea3
Sharper <sharp.bruce.lee@gmail.com>
2016-09-01 17:45:05 +0800

    react webpack

---
+++ server/petshop-admin/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            // <div>
+                <p> Yo, React </p>
+            // </div>
+        )
+    }
+}
+
+render(<App />, document.getElementById('content'));
