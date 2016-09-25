647aa0c1c0e0cb157ce57ad3a31b29bb77a2fe03
Sharper <sharp.bruce.lee@gmail.com>
2016-09-25 18:03:55 +0800

    client

---
+++ client/petshop/src/touchableButton.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+    TouchableHighlight,
+    Text,
+    Alert,
+    StyleSheet
+} from 'react-native';
+
+export default class TouchableButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pressed: false
+        }
+    }
+    render() {
+        return (
+    <TouchableHighlight onPress={
+        ()=> {
+            // Alert.alert(
+            //     `你点击了按钮`,
+            //     'Hello World！',
+            //     [
+            //         {text: '以后再说', onPress: () => console.log('Ask me later pressed')},
+            //         {text: '取消', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+            //         {text: '确定', onPress: () => console.log('OK Pressed')},
+            //     ]
+            // )
+        }
+    } style={[styles.button, this.state.pressed ? {backgroundColor: 'green'} : {}]}
+    onHideUnderlay={()=>{this.setState({pressed: false})}}
+    onShowUnderlay={()=>{this.setState({pressed: true})}}>
+        <Text>Button</Text>
+    </TouchableHighlight>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        borderColor: 'blue',
+        borderWidth: 1,
+        borderRadius: 5
+    },
+});
