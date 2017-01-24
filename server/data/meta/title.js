085bf39fd09286b72a8f83dd196a5a78300d3f65
Uncle Charlie <lixiangdong@haoqix.com>
2017-01-24 08:24:49 +0800

    es201x, gulp, eslint

--- server/data/meta/title.js
+++ server/data/meta/title.js
@@ -1,31 +1,29 @@
-var _ = require('lodash'),
-    config = require('../../config');
+import * as _ from 'lodash'
+import config from '../../config'
 
-function getTitle(data, root) {
-    var title = '',
-        context = root ? root.context : null,
-        blog = config.theme,
-        pagination = root ? root.pagination : null,
-        pageString = '';
+export default function getTitle(data, root) {
+  let title = ''
+  const context = root ? root.context : null
+  const blog = config.theme
+  const pagination = root ? root.pagination : null
+  let pageString = ''
 
-    if (pagination && pagination.total > 1) {
-        pageString = ' - Page ' + pagination.page;
-    }
-    if (data.meta_title) {
-        title = data.meta_title;
-    } else if (_.includes(context, 'home')) {
-        title = blog.title;
-    } else if (_.includes(context, 'author') && data.author) {
-        title = data.author.name + pageString + ' - ' + blog.title;
-    } else if (_.includes(context, 'tag') && data.tag) {
-        title = data.tag.meta_title || data.tag.name + pageString + ' - ' + blog.title;
-    } else if ((_.includes(context, 'post') || _.includes(context, 'page')) && data.post) {
-        title = data.post.meta_title || data.post.title;
-    } else {
-        title = blog.title + pageString;
-    }
+  if (pagination && pagination.total > 1) {
+    pageString = ` - Page ${pagination.page}`;
+  }
+  if (data.meta_title) {
+    title = data.meta_title;
+  } else if (_.includes(context, 'home')) {
+    title = blog.title;
+  } else if (_.includes(context, 'author') && data.author) {
+    title = `${data.author.name}${pageString} - ${blog.title}`;
+  } else if (_.includes(context, 'tag') && data.tag) {
+    title = data.tag.meta_title || `${data.tag.name}${pageString} - ${blog.title}`;
+  } else if ((_.includes(context, 'post') || _.includes(context, 'page')) && data.post) {
+    title = data.post.meta_title || data.post.title;
+  } else {
+    title = blog.title + pageString;
+  }
 
-    return (title || '').trim();
+  return (title || '').trim();
 }
-
-module.exports = getTitle;
