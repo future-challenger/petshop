2bb7de4e520682fe9f97feecf031cb3e63517a3d
Uncle Charlie <lixiangdong@haoqix.com>
2017-01-30 22:16:27 +0800

    placeholder helpers path

---
+++ server/helpers/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Uncle Charlie, 2017/01/29
+ * 
+ * @flow
+ */
+
+/**
+ * This function is just placeholder for now
+ */
+export default function loadCoreHelpers(hbsApp) {
+  console.log('=====>config helpers')
+}
