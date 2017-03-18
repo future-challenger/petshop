1193e9607c718f36918bf3c31b3378ca9a575a09
Uncle Charlie <lixiangdong@haoqix.com>
2017-03-18 16:02:40 +0800

    petshp-admin

---
+++ client/petshop-admin/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;
